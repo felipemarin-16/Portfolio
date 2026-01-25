6c6d5c21d8ad7208ebf7ee320e87681fba359b6b
Felipe Marin <156136792+felipemarin-16@users.noreply.github.com>
2026-01-24 23:39:04 -0700

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eae4d4218e654017"; }
+/******/ 		__webpack_require__.h = function() { return "13b4f81851a3abd8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
